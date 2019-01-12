9e5bc924949e156a550f012912f74a8e5921d6bc
jingyangtai <835007220@qq.com>
2019-01-12 23:18:39 +0800

    logback

--- core/src/main/java/com/example/serviceImpl/user/UserServiceImpl.java
+++ core/src/main/java/com/example/serviceImpl/user/UserServiceImpl.java
@@ -3,15 +3,20 @@ package com.example.serviceImpl.user;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.example.beans.user.User;
 import com.example.service.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public User queryById(Long id) {
         User user=new User();
         user.setId(1L);
         user.setName("uubird");
         user.setAge(25);
+        logger.info("queryById"+user);
         return user;
     }
 }
